8c725c3eddd5813fca65095db90aa99dc39d23d8
Tomasz Skrzypek <tomskr3.TS@gmail.com>
2026-03-25 19:07:02 +0100

    Initial commit

---
+++ HigienaHarmonogramBackend/src/main/java/pl/tomskr/higienaharmonogrambackend/HigienaHarmonogramBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.tomskr.higienaharmonogrambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigienaHarmonogramBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HigienaHarmonogramBackendApplication.class, args);
+    }
+
+}
---
+++ HigienaHarmonogramBackend/src/test/java/pl/tomskr/higienaharmonogrambackend/HigienaHarmonogramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomskr.higienaharmonogrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HigienaHarmonogramBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
